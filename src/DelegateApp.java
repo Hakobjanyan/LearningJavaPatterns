ad14748dc8e34e1696efae600a81c0427a25c662
hakobjanyan <ars.hakobjanyan@gmail.com>
2017-08-10 15:42:29 +0400

    Класс B делегирует класс A

--- src/DelegateApp.java
+++ src/DelegateApp.java
@@ -2,4 +2,22 @@
  * Created by Arsen on 10.08.2017.
  */
 public class DelegateApp {
+    public static void main(String[] args)
+    {
+        B b = new B();
+        b.f();
+    }
+}
+
+class A{
+    void f(){
+        System.out.println("f()");
+    }
+}
+
+class B{
+    A a = new A();
+    void f(){
+        a.f();
+    }
 }
