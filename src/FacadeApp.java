18876d254b8464979f3e0f046c8d4755e3209958
hakobjanyan <ars.hakobjanyan@gmail.com>
2017-08-10 17:48:03 +0400

    FacedeApp step1

---
+++ src/FacadeApp.java
@@ -0,0 +1,54 @@
+/**
+ * Created by Arsen on 10.08.2017.
+ */
+public class FacadeApp {
+    public static void main(String[] args) {
+        Power power = new Power();
+        power.on();
+
+        DVDRom dvdRom = new DVDRom();
+        dvdRom.load();
+
+        HDD hdd = new HDD();
+        hdd.copyFromDVD(dvdRom);
+    }
+}
+
+
+
+class Power {
+    void on() {
+        System.out.println("Включение питания");
+    }
+
+    void off() {
+        System.out.println("Выключение питания");
+    }
+}
+
+class DVDRom {
+    private boolean data = false;
+
+    public boolean hasData() {
+        return data;
+    }
+
+    void load() {
+        data = true;
+    }
+
+    void unload() {
+        data = false;
+    }
+}
+
+class HDD {
+    void copyFromDVD(DVDRom dvd) {
+        if (dvd.hasData()) {
+            System.out.println("Происходит копироваие данных с диска");
+        } else {
+            System.out.println("Вставьте диск с данными");
+
+        }
+    }
+}
